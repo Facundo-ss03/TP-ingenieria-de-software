e04238d94a40d509acea17809d9308b66ae80bc7
facundo <facundomartinsosa205@gmail.com>
2026-04-19 00:50:21 -0300

    mejora del estilo de la página y el diseño responsive, además, ahora hay un panel de búsqueda y resultados al lado del mapa.

---
+++ model.js
@@ -0,0 +1,17 @@
+
+const talleres = [
+
+    taller = {
+        name: "Lo de Pedro",
+        direction: "papapa"
+    },
+    taller2 = {
+        name: "Lo de Pedro",
+        direction: "papapa"
+    },
+    taller3 = {
+        name: "Lo de Pedro",
+        direction: "papapa"
+    }
+
+];
